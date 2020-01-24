748ec11c3dff468e534967709c3abf3878d215cc
vpapandrea <papandreavittorio@gmail.com>
2020-01-24 18:19:24 -0500

    removed agreement

--- src/main.c
+++ src/main.c
@@ -15,16 +15,6 @@ typedef struct {
   char* pcap_feed_file;
 } cli;
 
-void agreement() {
-  printf("riski (C) Vittorio Papandrea\n");
-  printf("Usage of this program implies that the owner(s) of this project\n");
-  printf("are not liable for any losses as the result of using riski as a\n");
-  printf("tool for making decisions in any tradable market. This program \n");
-  printf("is given free of warentee and liability.\n");
-  printf("Please Trade Responsibilly\n");
-  printf("\n\n");
-}
-
 /**
  * Parses the comand line arguments
  */
