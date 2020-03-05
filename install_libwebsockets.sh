ca5bae1edd6be68df4c234b0b53647b9cc80b149
vpapandrea <papandreavittorio@gmail.com>
2020-03-05 00:38:23 -0500

    attempting azure pipeline fix (for mac!)

--- install_libwebsockets.sh
+++ install_libwebsockets.sh
@@ -4,7 +4,33 @@ git clone https://libwebsockets.org/repo/libwebsockets
 cd libwebsockets
 mkdir build
 cd build
-cmake ..
-sudo make install
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Mac OS X'
+     cmake -DOPENSSL_ROOT_DIR=/usr/local/ssl -DOPENSSL_LIBRARIES=/usr/local/ssl/lib ..
+     ;;
+
+   Linux)
+     echo 'Linux'
+     cmake ..
+     sudo make install
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+     echo 'MS Windows'
+     ;;
+
+   # Add here more strings to compare
+   # See correspondence table at the bottom of this answer
+
+   *)
+     echo 'Other OS'
+     cmake ..
+     sudo make install
+     ;;
+esac
+
 cd ..
 rm -rf libwebsockets
