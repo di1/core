f141c8ea9ec5d478681dfa8af417b392240d54c2
vpapandrea <papandreavittorio@gmail.com>
2020-06-04 12:29:59 -0400

    ignore libwebsocket.h warnings

--- inc/server/server.h
+++ inc/server/server.h
@@ -2,7 +2,13 @@
 #define SERVER_
 
 #include <iex/iex.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
 #include <libwebsockets.h>
+#pragma clang diagnostic pop
+
 #include <server/message_parser.h>
 #include <signal.h>
 #include <string.h>
