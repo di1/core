abca61acec900cfacde7044d2e45e236d9beb122
vpapandrea <papandreavittorio@gmail.com>
2020-06-04 12:38:35 -0400

    ignore libwebsocket.h warnings

--- inc/server/server.h
+++ inc/server/server.h
@@ -6,6 +6,9 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdocumentation"
 #pragma clang diagnostic ignored "-Wreserved-id-macro"
+#pragma clang diagnostic ignored "-Wpadded"
+#pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma clang diagnostic ignored "-Wduplicate-enum"
 #include <libwebsockets.h>
 #pragma clang diagnostic pop
 
